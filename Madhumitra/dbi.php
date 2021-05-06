2c108a4f521fb4601268ed0e9cfa8dac984339b5
poojakhore <83715074+poojakhore@users.noreply.github.com>
2021-05-06 20:23:33 +0530

    Delete Madhumitra directory

--- Madhumitra/dbi.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	// mysql, user,pwd,database
- $con = mysqli_connect("localhost","root","","madhumitra_db") or die(mysqli_error($con));
-
-?>
--- Madhumitra/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-
-// destroy all session variables
-session_destroy();
-
-header("location:login.php");
-?>
